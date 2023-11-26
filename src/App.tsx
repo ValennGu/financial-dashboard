b73d989df5620368dde56da97503ec05340f298f
Valentin <valentin.g.l@gmail.com>
2023-11-26 18:18:26 +0100

    Remove unnecesary sm tag

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ function App() {
         reverseOrder={false}
       />
       <Grid numItems={5} className="gap-2">
-        <Col numColSpanLg={1} numColSpanSm={0}/>
-        <Col numColSpanLg={3} numColSpanSm={5}>
+        <Col numColSpanLg={1} numColSpan={0}/>
+        <Col numColSpanLg={3} numColSpan={5}>
           <SavingsLineChart year="2023"/>
         </Col>
-        <Col numColSpanLg={1} numColSpanSm={0}/>
+        <Col numColSpanLg={1} numColSpan={0}/>
       </Grid>
     </>
   );
